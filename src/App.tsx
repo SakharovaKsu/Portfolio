59fcecb4858320b870905ed00854167c7d14b827
SakharovaKsu <nagis38@gmail.com>
2023-07-16 15:03:47 +0500

    Добавление структуры Skills

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import Skills from './Components/Skills/Skills';
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <Main/>
+        <Skills/>
     </div>
   );
 }
