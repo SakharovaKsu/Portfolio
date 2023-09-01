ffad676e226c07de79d915984f8432a9e31bc226
SakharovaKsu <nagis38@gmail.com>
2023-09-01 21:48:30 +0300

    Подключение шрифтов, стилизация компонента main

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import s from './Main.module.css'
 import '../../index.css'
 import Button from '../Elements/Button';
+import photo from '../../img/photo.png';
 
 const Main = () => {
     return (
         <div className={'container'}>
             <div className={s.main}>
                 <div className={s.boxText}>
-                    <span>Привет, меня зовут</span>
-                    <h1 className={s.title}>Ксения Сахарова</h1>
-                    <p className={s.text}>Я front-end разработчик</p>
-                    <Button title={'Мое портфолио'}/>
+                    <span className={s.text}> Hello, <b>my name is</b></span>
+                    <h1 className={s.title}>Ksenia <b>Sakharova</b></h1>
+                    <p className={s.subtitle}>I am a front-end developer</p>
+                    <Button title={'My portfolio'}/>
                 </div>
-                <div className={s.photo}></div>
+                <div className={s.photo}>
+                    <img src={photo} alt={'Фото Ксении'}/>
+                </div>
             </div>
         </div>
     );
